ba062c6355f63afd3f9c73fd8da748fbad066d83
Human666 <68257361+VGCOKE@users.noreply.github.com>
2020-12-26 14:51:15 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@ print("Calculator | VG COKE") #title (optional)
 def cal(): #create a function
     num1 = int(input("Type in the first number: ")) #input number #int=integer
     num2 = int(input("Type in the second number: ")) #input number #int=integer #2nd number
-    print("1- plus, 2- minus, 3- times, 3- divide") #print out the options
+    print("1- plus, 2- minus, 3- times, 4- divide") #print out the options
     option = input("Type in the option: ")#input option
     if option =="1":
         print(num1, "+", num2, "=", num1 + num2)
